209b16e892dddb6c9d4685f2c95e1dc8f46ac421
Aymen Smati <ayman@Smatis-MBP.lan>
2021-07-08 00:17:36 +0100

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvbz8kO8mRut_O9jwtyrbUHAzKeUVlvqA",
+    authDomain: "slack-clone-841e6.firebaseapp.com",
+    projectId: "slack-clone-841e6",
+    storageBucket: "slack-clone-841e6.appspot.com",
+    messagingSenderId: "840632558200",
+    appId: "1:840632558200:web:563c1dd4e3b6ba9755838d",
+    measurementId: "G-LQZ561Q5GP"
+  };
